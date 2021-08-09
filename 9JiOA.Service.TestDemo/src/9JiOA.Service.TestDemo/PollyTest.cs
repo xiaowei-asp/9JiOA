8793454e30b197ac42d31abbe8c1607321f4be34
肖伟 <13379@9ji.com>
2021-08-09 13:30:01 +0800

    测试方法添加

---
+++ 9JiOA.Service.TestDemo/src/9JiOA.Service.TestDemo/PollyTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using Polly;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace _9JiOA.Service.TestDemo
+{
+    /// <summary>
+    /// Pooly测试
+    /// </summary>
+    public class PollyTest
+    {
+        [Test]
+        public void PollyRetry()
+        {
+            // Retry multiple times, calling an action on each retry 
+            // with the current exception and retry count
+            var result = string.Empty;
+            var i = 0;
+            Policy.HandleResult<bool>(string.IsNullOrWhiteSpace(result))
+                .Retry(3, (reponse, retryCount, context) =>
+                {
+                    //call back
+                    //錯誤發生後要做的事
+                    Console.WriteLine(i++);
+                    result = "1";
+                });
+        }
+    }
+}
